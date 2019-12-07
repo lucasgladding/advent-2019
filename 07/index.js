2967b880b7532ad94d3126a39cab569615a3e88d
Lucas Gladding <l.gladding@vehikl.com>
2019-12-07 18:10:05 -0500

    Implemented part 1.

--- 07/index.js
+++ 07/index.js
@@ -1,3 +1,36 @@
 const { run } = require('../05/index');
 
-module.exports = { run };
+function evaluate(program, sequence) {
+  return sequence.reduce((input, i) => {
+    return start([...program], [i, input]);
+  }, 0);
+}
+
+function start(program, input) {
+  const output = run(program, input);
+  return output[0];
+}
+
+// 0,1,2
+// 0,2,1
+// 1,0,2
+// 1,2,0
+// 2,0,1
+// 2,1,0
+
+function permutations(options) {
+  if (options.length === 1) {
+    return [options];
+  }
+  const output = [];
+  for (let i of options) {
+    const filt = options.filter(option => option !== i);
+    const perm = permutations(filt);
+    for (let j of perm) {
+      output.push([i, ...j]);
+    }
+  }
+  return output;
+}
+
+module.exports = { evaluate, permutations };
