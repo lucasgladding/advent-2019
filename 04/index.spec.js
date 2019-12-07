fcd0e3edd063e46344831ef3de60a5a160bd6064
Lucas Gladding <l.gladding@vehikl.com>
2019-12-06 22:37:02 -0500

    Updated tests.

--- 04/index.spec.js
+++ 04/index.spec.js
@@ -7,27 +7,27 @@ describe('04', () => {
   // 223450 does not meet these criteria (decreasing pair of digits 50).
   // 123789 does not meet these criteria (no double).
 
-  xit('example 1', () => {
-    expect(test.test(111111)).toEqual(true);
-  });
+  // it('example 1', () => {
+  //   expect(test.test(111111)).toEqual(true);
+  // });
 
-  xit('example 2', () => {
-    expect(test.test(223450)).toEqual(false);
-  });
+  // it('example 2', () => {
+  //   expect(test.test(223450)).toEqual(false);
+  // });
 
-  xit('example 3', () => {
-    expect(test.test(123789)).toEqual(false);
-  });
+  // it('example 3', () => {
+  //   expect(test.test(123789)).toEqual(false);
+  // });
 
-  xit('part 1', () => {
-    let counter = 0;
-    for (let i = 158126; i <= 624574; i++) {
-      if (test.test(i)) {
-        counter++;
-      }
-    }
-    expect(counter).toEqual(1665);
-  });
+  // it('part 1', () => {
+  //   let counter = 0;
+  //   for (let i = 158126; i <= 624574; i++) {
+  //     if (test.test(i)) {
+  //       counter++;
+  //     }
+  //   }
+  //   expect(counter).toEqual(1665);
+  // });
 
   // 112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
   // 123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
