6ec8c5f3fc9376351bdc55109f34bad5a5484c84
Lucas Gladding <l.gladding@vehikl.com>
2019-12-18 21:43:03 -0500

    Working on 16.2.

--- 16/index.js
+++ 16/index.js
@@ -1,10 +1,7 @@
-function decode(string) {
-  return string.split('').map(digit => parseInt(digit));
-}
-
 function loop(sequence, pattern, times) {
   let current = sequence;
   for (let i = 0; i < times; i++) {
+    console.log('loop', i);
     current = process(current, pattern);
   }
   return current;
@@ -18,7 +15,7 @@ function process(sequence, pattern) {
 }
 
 function process_element(sequence, pattern, element) {
-  const items = repeat(pattern, element + 1);
+  const items = inner_repeat(pattern, element + 1);
   return sequence.reduce((acc, item, index) => {
     return acc + item * get_item(items, index);
   }, 0);
@@ -29,11 +26,11 @@ function get_item(items, pos) {
   return items[index];
 }
 
-function repeat(items, times) {
+function inner_repeat(items, times) {
   return items.reduce((acc, item) => {
     const items = new Array(times).fill(item);
     return [...acc, ...items];
   }, []);
 }
 
-module.exports = { decode, loop, process };
+module.exports = { loop, process };
